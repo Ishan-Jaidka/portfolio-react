c500777651c3b2e54019912c28c3e7d647d1786d
Ishan Jaidka <ijaidka@gmail.com>
2023-01-18 23:02:55 -0800

    updated bio

--- src/views/aboutMe.js
+++ src/views/aboutMe.js
@@ -3,9 +3,9 @@ import HeadShot from "../components/headShot";
 import "./aboutMe.css";
 
 export default function AboutMe() {
-  const text = `I am a software engineer with 1 year of professional full stack development experience, 3 years of programming experience, and 6 years of customer service experience in the outdoor industry. I have a B.S. in Computer Science (cum laude) from Bellevue College.
+  const text = `I am a software engineer with 1 year of professional full stack development experience, 3 years of programming experience, and 6 years of customer service experience in the outdoor industry.
 
-    My passions include learning, interpersonal connections, and all things web development, automation, and robotics. I have experience with AWS, Node.js, React, Next.js, Javascript, Typescript, HTML/CSS, Java, C++, C#, Kotlin, Python, SQL, Arduino, and C.`;
+    My passions include learning, interpersonal connections, and all things web development, automation, and robotics. I have a B.S. in Computer Science cum laude and experience with AWS, Node.js, React, Next.js, Javascript, Typescript, HTML/CSS, Java, C++, C#, Kotlin, Python, SQL, Arduino, and C.`;
   return (
     <div className="aboutme">
       <div className="page-title">About Me</div>
