c0c0aec839b18ec24cc531a56a851e18bd5de629
Ishan Jaidka <ijaidka@gmail.com>
2023-01-21 02:25:40 -0800

    fix lint warning

--- src/views/projectPage.js
+++ src/views/projectPage.js
@@ -23,6 +23,7 @@ export default function ProjectPage() {
         setStatus("Error");
         console.error(err);
       });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
