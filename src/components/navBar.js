fe17dc5d715fefbcefd60b973af372dd8cc991d8
Ishan Jaidka <ijaidka@gmail.com>
2023-01-25 01:49:23 -0800

    mui navbar

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,18 +1,49 @@
+import {
+  AppBar,
+  Button,
+  Divider,
+  IconButton,
+  Stack,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import React from "react";
 import "./navBar.css";
 
 export default function NavBar() {
   return (
-    <div className="navbar">
-      <a className="nav-link" href="/">
-        Home
-      </a>
-      <a className="nav-link" href="/projects">
-        Projects
-      </a>
-      <a className="nav-link" href="/about">
-        About Me
-      </a>
-    </div>
+    <AppBar position="fixed" sx={{ background: "teal" }}>
+      <Toolbar>
+        <IconButton href="/" size="large" edge="start" sx={{ mr: 2 }}>
+          <img className="logo" src="/ishan-circle.png" alt="me" />
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{
+              flexGrow: 1,
+              color: "white",
+            }}
+          >
+            Ishan Jaidka
+          </Typography>
+        </IconButton>
+        <Stack
+          direction="row"
+          spacing={2}
+          marginLeft="auto"
+          divider={<Divider orientation="vertical" flexItem />}
+        >
+          <Button sx={{ color: "white" }} href="/projects">
+            Projects
+          </Button>
+          <Button sx={{ color: "white" }} href="/experience">
+            Experience
+          </Button>
+          <Button sx={{ color: "white" }} href="/about">
+            About Me
+          </Button>
+        </Stack>
+      </Toolbar>
+    </AppBar>
   );
 }
