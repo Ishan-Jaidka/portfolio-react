a1b017b0e3e70f69a18d07ec807bcfc4a0a1310c
Ishan Jaidka <ijaidka@gmail.com>
2023-03-03 00:06:54 -0600

    update homepage

--- src/components/myName.js
+++ src/components/myName.js
@@ -1,5 +1,21 @@
 import React from "react";
+import "./myName.css";
 
-export default function MyName({ name }) {
-  return <div className="page-title">{name}</div>;
+export default function MyName({ name, title }) {
+  return (
+    <div>
+      <h3 className="about-intro">Hi! I'm</h3>
+      <a className="about-name" href="/about">
+        {name}
+      </a>
+
+      <h3 className="about-title-intro">
+        And I'm a{" "}
+        <a href="/experience" className="about-title">
+          {title}
+        </a>
+        .
+      </h3>
+    </div>
+  );
 }
--- src/views/homePage.js
+++ src/views/homePage.js
@@ -1,13 +1,11 @@
 import React from "react";
-import HeadShot from "../components/headShot";
 import MyName from "../components/myName";
 
 export default function HomePage() {
   return (
     <div className="App">
       <header className="App-header">
-        <MyName name="Ishan Jaidka" />
-        <HeadShot />
+        <MyName name="Ishan Jaidka" title="Software Engineer" />
       </header>
     </div>
   );
