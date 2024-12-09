f23b1b9cc90258cd78b15c36e1e0ef6bb92c5447
Ishan Jaidka <ijaidka@gmail.com>
2024-12-09 14:47:13 -0800

    updated about me

--- src/views/aboutMe.js
+++ src/views/aboutMe.js
@@ -3,14 +3,16 @@ import HeadShot from "../components/headShot";
 import "./aboutMe.css";
 
 export default function AboutMe() {
-  const text = `I am a software engineer with 1 year of professional full stack development experience, 3 years of programming experience, and 6 years of customer service experience in the outdoor industry.`;
-  const text2 = `My passions include learning, interpersonal connections, and all things web development, automation, and robotics. I have a B.S. in Computer Science cum laude and experience with AWS, Node.js, React, Next.js, Javascript, Typescript, HTML/CSS, Java, C++, C#, Kotlin, Python, SQL, Arduino, and C.`;
+  const text = `Hey there! 👋 I'm a software engineer who's just as comfortable in the great outdoors as I am behind a computer screen. With a background in full-stack development and a knack for creating killer automation projects, I bring a unique blend of tech skills and a passion for customer service to the table.`;
+  const text2 = `Whether I'm diving into the world of web user interfaces or geeking out over robotics, I love the learning process and thrive on making meaningful connections. Armed with a B.S. in Computer Science, my toolbox includes AWS, Node.js, React, Next.js, Javascript, Typescript, HTML/CSS, Java, C++, C#, Kotlin, Python, SQL, Arduino, and C.`;
+  const text3 = `Let's chat code, share outdoor adventure stories, or discuss the latest tech trends. I'm always up for a good conversation!`;
   return (
     <div className="aboutme">
       <div className="page-title">About Me</div>
       <HeadShot />
       <div className="page-description">{text}</div>
       <div className="aboutme-paragraph">{text2}</div>
+      <div className="aboutme-paragraph">{text3}</div>
     </div>
   );
 }
